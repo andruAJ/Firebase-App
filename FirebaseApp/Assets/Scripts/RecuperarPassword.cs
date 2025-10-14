563c4bb2771a1aad106f074b2fb931e2fa988086
andruAJ <giraldoandresjuan0405@gmail.com>
2025-10-14 14:54:33 -0500

    adelantos email recuperacion

--- FirebaseApp/Assets/Scripts/RecuperarPassword.cs
+++ FirebaseApp/Assets/Scripts/RecuperarPassword.cs
@@ -1,16 +1,42 @@
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
 using UnityEngine;
 
 public class RecuperarPassword : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private FirebaseAuth auth;
+
     void Start()
     {
-        
+        auth = FirebaseAuth.DefaultInstance;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetPassword()
     {
-        
+        FirebaseUser user = auth.CurrentUser;
+        string email = user.Email;
+        auth.SendPasswordResetEmailAsync(email).ContinueWith(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Hubo un error mandando el correo");
+                return;
+            }
+        });
+
+        //user.UpdatePasswordAsync().ContinueWith(task =>
+        //{
+        //    if (task.IsCanceled)
+        //    {
+        //        return;
+        //    }
+        //    if (task.IsFaulted)
+        //    {
+        //        return;
+        //    }
+
+        //});
+
     }
 }
