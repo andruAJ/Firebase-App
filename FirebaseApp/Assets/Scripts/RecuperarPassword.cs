c87e4a204e9d45bacc113eb93b5b679d49b13b49
andruAJ <giraldoandresjuan0405@gmail.com>
2025-10-15 14:46:23 -0500

    Recuperación password completamente funcional

--- FirebaseApp/Assets/Scripts/RecuperarPassword.cs
+++ FirebaseApp/Assets/Scripts/RecuperarPassword.cs
@@ -2,20 +2,38 @@ using Firebase;
 using Firebase.Auth;
 using Firebase.Database;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RecuperarPassword : MonoBehaviour
 {
+    private UIDocument uiDocument;
+    private TextField emailField;
+    private Button cambiarCartaButton; 
+    private Button resetButton;
+
     private FirebaseAuth auth;
 
     void Start()
     {
+        uiDocument = GetComponent<UIDocument>();
+        emailField = uiDocument.rootVisualElement.Q<TextField>("EmailRecuperar_TextField");
+        cambiarCartaButton = uiDocument.rootVisualElement.Q<Button>("Cambiar_Button");
+        resetButton = uiDocument.rootVisualElement.Q<Button>("Recuperar_Button");
+
+        cambiarCartaButton.RegisterCallback<ClickEvent>(ev => {
+            var loginCard = uiDocument.rootVisualElement.Q<VisualElement>("LogIn_Card");
+            var recuperarCard = uiDocument.rootVisualElement.Q<VisualElement>("Recuperar_Card");
+            loginCard.style.display = DisplayStyle.None;
+            recuperarCard.style.display = DisplayStyle.Flex;
+        });
+        resetButton.RegisterCallback<ClickEvent>(ev => ResetPassword());
+
         auth = FirebaseAuth.DefaultInstance;
     }
 
     public void ResetPassword()
     {
-        FirebaseUser user = auth.CurrentUser;
-        string email = user.Email;
+        string email = emailField.text;
         auth.SendPasswordResetEmailAsync(email).ContinueWith(task =>
         {
             if (task.IsFaulted)
@@ -24,19 +42,5 @@ public class RecuperarPassword : MonoBehaviour
                 return;
             }
         });
-
-        //user.UpdatePasswordAsync().ContinueWith(task =>
-        //{
-        //    if (task.IsCanceled)
-        //    {
-        //        return;
-        //    }
-        //    if (task.IsFaulted)
-        //    {
-        //        return;
-        //    }
-
-        //});
-
     }
 }
